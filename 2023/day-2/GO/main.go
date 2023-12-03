94e0c99f3f4b1e025c84b7ecd300f2ea01d4c145
SavioNicodemos <nicodemosgcosta@gmail.com>
2023-12-03 02:50:50 +0000

    Day 2 part two finished

--- 2023/day-2/GO/main.go
+++ 2023/day-2/GO/main.go
@@ -14,11 +14,13 @@ var possibleConfigs = map[string]int{
 }
 
 func main() {
-	lines := readInput(("input.txt"))
+	lines := readInput("input.txt")
 
 	partOneResult := partOne(lines)
+	partTwoResult := partTwo(lines)
 
 	fmt.Println("Part one result:", partOneResult)
+	fmt.Println("Part two result:", partTwoResult)
 }
 
 func readInput(file string) []string {
@@ -29,14 +31,6 @@ func readInput(file string) []string {
 
 	return strings.Split(string(rawFile), "\n")
 }
-
-/*
-Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
-Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
-Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
-Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
-Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
-*/
 
 func getValues(line string) map[string]int {
 	values := strings.Split(line, ":")
@@ -88,4 +82,23 @@ func partOne(lines []string) int {
 		}
 	}
 	return idSum
+}
+
+func partTwo(lines []string) int {
+	var acc int
+
+	for _, line := range lines {
+		colorValues := getValues(line)
+		curr := 0
+		for _, value := range colorValues {
+			if curr == 0 {
+				curr = value
+				continue
+			}
+			curr *= value
+		}
+		acc += curr
+	}
+
+	return acc
 }
